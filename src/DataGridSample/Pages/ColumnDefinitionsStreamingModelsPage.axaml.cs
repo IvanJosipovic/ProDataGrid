6eed6fee241c7db39782bcf32cd2f1f933ed0417
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-02-23 13:52:00 +0100

    Fix streaming models page DataContext null timing

--- src/DataGridSample/Pages/ColumnDefinitionsStreamingModelsPage.axaml.cs
+++ src/DataGridSample/Pages/ColumnDefinitionsStreamingModelsPage.axaml.cs
@@ -1,31 +1,50 @@
 using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
 using DataGridSample.ViewModels;
 
 namespace DataGridSample.Pages
 {
     public partial class ColumnDefinitionsStreamingModelsPage : UserControl
     {
+        private DataGrid? _streamingGrid;
+
         public ColumnDefinitionsStreamingModelsPage()
         {
             InitializeComponent();
-            AttachedToVisualTree += (_, _) => DataContext ??= new ColumnDefinitionsStreamingModelsViewModel();
+            AttachedToVisualTree += OnAttachedToVisualTree;
             DataContextChanged += OnDataContextChanged;
-            OnDataContextChanged(this, EventArgs.Empty);
+            ApplyFastPathOptions();
+        }
+
+        private void OnAttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
+        {
+            DataContext ??= new ColumnDefinitionsStreamingModelsViewModel();
+            ApplyFastPathOptions();
         }
 
         private void OnDataContextChanged(object? sender, EventArgs e)
         {
-            if (DataContext is ColumnDefinitionsStreamingModelsViewModel viewModel)
-            {
-                StreamingGrid.FastPathOptions = viewModel.FastPathOptions;
-            }
+            ApplyFastPathOptions();
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+            _streamingGrid = this.FindControl<DataGrid>("StreamingGrid");
+        }
+
+        private void ApplyFastPathOptions()
+        {
+            var grid = _streamingGrid ??= this.FindControl<DataGrid>("StreamingGrid");
+            if (grid == null)
+            {
+                return;
+            }
+
+            grid.FastPathOptions = (DataContext as ColumnDefinitionsStreamingModelsViewModel)?.FastPathOptions;
         }
     }
 }
